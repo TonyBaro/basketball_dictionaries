5405103123f90c986a92dee62b1730348bff697d
TonyB <tonybaromagic@gmail.com>
2022-05-12 20:07:07 -0400

    done

--- basketball_dictionary.py
+++ basketball_dictionary.py
@@ -28,6 +28,9 @@ class Player:
         self.position = data["position"]
         self.team = data["team"]
 
+    def __repr__(self):
+        return f'Player:{self.name}'
+
 player1 = Player(kevin)
 player2 = Player(jason)
 player3 = Player(kyrie)
@@ -36,3 +39,46 @@ print (player1.name)
 print(player2.name)
 print(player3.name)
 
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32, "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33, "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32, "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+    	"name": "", 
+    	"age":16, 
+    	"position": "P", 
+    	"team": "en"
+    }
+]
+new_team= []
+
+for x in players:
+	player = Player(x)
+	new_team.append(player)
+
+print(new_team)
